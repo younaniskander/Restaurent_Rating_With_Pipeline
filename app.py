30c54296d278b1fd7e2df6deaf2ea6f29a6f9522
Younan Iskander <87044703+younaniskander@users.noreply.github.com>
2023-05-17 01:50:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st 
 import joblib
 
-regressor=joblib.load('/kaggle/working/RestaurentRatingWithPipeline.pkl')
+regressor=joblib.load('/kaggle/working/RestaurentPipeline.pkl')
 
 
 def welcome():
